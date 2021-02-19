b7e23226a519429afcf526f4cb68a0e5b66457a8
srkvodnala <srkrao547@gmail.com>
2021-02-19 15:49:40 -0600

    add maven archetype

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # maven-java-app
+
+### mvn archetype:generate 
+- This command Generates a new project from an archetype
+- 
---
+++ src/main/java/com/vodnala/shiva/bigdata/App.java
@@ -0,0 +1,13 @@
+package com.vodnala.shiva.bigdata;
+
+/**
+ * Hello Northwest!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Northwest!" );
+    }
+}
